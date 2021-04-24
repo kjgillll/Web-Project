5d94b23161a70b24ab1cd8d057970908f3034126
Dexter/Flarezard <drdexterslaboratory@gmail.com>
2021-04-24 17:41:29 +0000

    added todict methods

--- models.py
+++ models.py
@@ -9,6 +9,16 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(120))
+    records = db.relationship('Record', backref='user', lazy=True)
+    
+    def toDict(self):
+      return {
+        "id": self.id,
+        "username": self.username,
+        "email": self.email,
+        "records": self.records,
+        "password": self.password
+      }
 
 # class Logs(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -26,10 +36,20 @@ class User(UserMixin, db.Model):
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
-    datetime = db.Column(db.DateTime,nullable = False)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
     bloodsugar = db.Column(db.Integer, nullable = False)
     insulinlevels = db.Column(db.Integer, nullable = True)
     comments = db.Column(db.String(255), nullable = True)
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "userid": self.userid,
+            'created': self.created.strftime("%m/%d/%Y, %H:%M:%S"),
+            "bloodsugar": self.bloodsugar,
+            "insulinlevels": self.insulinlevels,
+            "comments": self.comments           
+        }
 
 
 
