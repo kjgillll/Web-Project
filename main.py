b71bed948de113c6ef271c9d0a22ecec297421ad
kjgillll <keronn.gill@my.uwi.edu>
2021-04-25 05:12:46 +0000

    Try to Fix Stuff

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def load_user(user_id):
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    password = PasswordField('password',validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
